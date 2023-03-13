cc5ba7651dfad8d64f26d9aa570e065ef0a8ed67
Florian Ellis <florian.ellis@qonto.com>
2023-03-13 10:33:02 +0100

    Reformat all files with black

--- pint_api/apps.py
+++ pint_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PintApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pint_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pint_api"
