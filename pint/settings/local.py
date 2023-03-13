5c35767013aa3cb46a9e4bc1cb5a613020e52d92
Florian Ellis <florian.ellis@qonto.com>
2023-03-13 15:55:18 +0100

    Split settings between local and production and add local settings on allowed CORS headers

---
+++ pint/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = "django-insecure-37cf#7qc08t*z@ojq=on=9kv7s_=ase1x6$_@^a3mgx+s#l_p+"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
---
+++ pint/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = False
+
+# ALLOWED_HOSTS = [...]
+
+# SECRET_KEY = [...]
+
+# CORS_ALLOWED_ORIGINS = [...]
+
+# DATABASES = { ... }
