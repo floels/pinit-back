9aaf627bcef19d6303ab00fd4b9a8bbe7b48a35e
Florian Ellis <florian.ellis@qonto.com>
2023-03-13 10:28:54 +0100

    Add test for edge cases of JWT obtain and refresh

--- pint_api/tests/test_authentication.py
+++ pint_api/tests/test_authentication.py
@@ -15,7 +15,7 @@ class AuthenticationTests(APITestCase):
           password=self.user_password
       )
 
-    def test_obtain_refresh_token(self):
+    def test_obtain_refresh_jw_token_happy_path(self):
         """
         Ensure we can obtain and refresh a JWT when providing valid credentials.
         """
@@ -33,3 +33,25 @@ class AuthenticationTests(APITestCase):
         self.assertEqual(response_refresh.status_code, status.HTTP_200_OK)
         refreshed_access_token = response_refresh.data['access']
         assert bool(refreshed_access_token)
+
+    def test_obtain_jw_token_wrong_credentials(self):
+        """
+        Ensure we don't obtain a JWT when providing invalid credentials.
+        """
+        data_wrong_username = { 'username': 'wrong_username', 'password': 'somePa$$word'}
+        response_wrong_username = self.client.post('/api/token/', data_wrong_username, format='json')
+
+        self.assertEqual(response_wrong_username.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        data_wrong_password = { 'username': self.user_username, 'password': 'somePa$$word'}
+        response_wrong_password = self.client.post('/api/token/', data_wrong_password, format='json')
+
+        self.assertEqual(response_wrong_password.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    def test_refresh_jw_token_wrong_refresh(self):
+        """
+        Ensure we don't obtain a refreshed JWT when providing a wrong refresh token.
+        """
+        response = self.client.post('/api/token/refresh/', {'refresh': 'wrong.refreshToken'}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
