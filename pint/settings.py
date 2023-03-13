35dde0ac7f0e271bcf98d5941dd1ee0f87507edc
Florian Ellis <florian.ellis@qonto.com>
2023-03-13 08:43:12 +0100

    Update settings.py

--- pint/settings.py
+++ pint/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pint_api.apps.PintApiConfig'
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'pint_api.apps.PintApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
