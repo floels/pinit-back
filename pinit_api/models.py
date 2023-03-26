519152fc2ff92de4f10688c77a95c4cc5e53a4b0
Florian Ellis <florian.ellis@qonto.com>
2023-03-26 21:56:18 +0200

    Create custom User model

--- pinit_api/models.py
+++ pinit_api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .utils.user_manager import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    # See https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#specifying-a-custom-user-model
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField(null=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
---
+++ pinit_api/views/signup_doc.py
@@ -0,0 +1,69 @@
+from drf_yasg import openapi
+
+SWAGGER_SCHEMAS = {
+    "SignupView": {
+        "operation_description": "Takes sign-up data and returns a pair of access and refresh token if sign-up was successful.",
+        "tags": ["Sign-up"],
+        "request_body": openapi.Schema(
+            type="object",
+            properties={
+                "email": openapi.Schema(type="string", format="email"),
+                "password": openapi.Schema(type="string", format="password"),
+                "birthdate": openapi.Schema(type="string", format="date"),
+            },
+            required=["username", "password", "birthdate"],
+            example={
+                "username": "john.doe@example.com",
+                "password": "J0hnDO€sPA$$W0RD",
+                "birthdate": "1970-01-01",
+            },
+        ),
+        "responses": {
+            200: openapi.Response(
+                description="Successful sign-in",
+                schema=openapi.Schema(
+                    type="object",
+                    properties={
+                        "access": openapi.Schema(
+                            type="string", description="Access token", min_length=1
+                        ),
+                        "refresh": openapi.Schema(
+                            type="string", description="Refresh token", min_length=1
+                        ),
+                    },
+                ),
+                examples={
+                    "application/json": {
+                        "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc4OTk0NTAwLCJpYXQiOjE2Nzg5OT",
+                        "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY3OTA4MDYwMCwiaWF0IjoxNjc4OTk0MjAwLCJqdGkiOiJlYTcyNTFjNzRmODU0YWFhYmFkNTI5MDBmMTBhOTQ2YiIsInVzZXJfaWQiOjF9.UgC1da2o4bt_K3tqEIQrRwkRx6au1TCK8ftosYJB_cw",
+                    }
+                },
+            ),
+            401: openapi.Response(
+                description="Invalid username or password",
+                schema=openapi.Schema(
+                    type="object",
+                    properties={
+                        "errors": openapi.Schema(
+                            type="array",
+                            items=openapi.Schema(
+                                type="object",
+                                properties={
+                                    "code": openapi.Schema(
+                                        type="string",
+                                        description="Error code",
+                                        min_length=1,
+                                        enum=["invalid_username", "invalid_password"],
+                                    )
+                                },
+                            ),
+                        )
+                    },
+                ),
+                examples={
+                    "application/json": {"errors": [{"code": "invalid_username"}]}
+                },
+            ),
+        },
+    }
+}
