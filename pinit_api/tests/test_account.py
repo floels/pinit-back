3ffb6cf893618d4665a91c2f5d228a9eced3b154
Florian Ellis <florian.ellis@qonto.com>
2023-05-06 14:18:30 +0200

    Update /user-details tests

--- pinit_api/tests/test_account.py
+++ pinit_api/tests/test_account.py
@@ -1,5 +1,6 @@
+from datetime import datetime, timedelta
 from rest_framework.test import APITestCase, APIClient
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework import status
 
 from ..models import User
@@ -36,7 +37,35 @@ class AccountTests(APITestCase):
         self.assertEqual(response_data["first_name"], self.user.first_name)
         self.assertEqual(response_data["last_name"], self.user.last_name)
 
-    def test_user_unauthenticated(self):
+    def test_user_details_no_access_token(self):
+        response = self.client.get("/api/user-details/")
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.assertEqual(
+            response.json()["errors"],
+            [{"code": ERROR_CODE_UNAUTHORIZED}],
+        )
+
+    def test_user_details_expired_access_token(self):
+        # Create expired token for the test user
+        access_token = AccessToken.for_user(self.user)
+        access_token.set_exp(from_time=datetime.now() - timedelta(minutes=10))
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {str(access_token)}")
+
+        response = self.client.get("/api/user-details/")
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.assertEqual(
+            response.json()["errors"],
+            [{"code": ERROR_CODE_UNAUTHORIZED}],
+        )
+
+    def test_user_details_invalid_access_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer invalid_token")
+
         response = self.client.get("/api/user-details/")
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
