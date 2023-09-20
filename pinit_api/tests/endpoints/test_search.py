47b4e1728fbdc6da3f66de061f210f0db0fa48de
Florian Ellis <florian.ellis@qonto.com>
2023-09-20 06:48:14 +0200

    Implement search autocomplete endpoint

--- pinit_api/tests/endpoints/test_search.py
+++ pinit_api/tests/endpoints/test_search.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from ..testing_utils import UserFactory
 from pinit_api.utils.constants import ERROR_CODE_UNAUTHORIZED
+from pinit_api.views.search import ERROR_CODE_MISSING_SEARCH_PARAMETER
 
 
 class SearchTests(APITestCase):
@@ -32,11 +33,25 @@ class SearchTests(APITestCase):
         self.assertGreaterEqual(len(response_results), 1)
 
     def test_get_accounts_no_access_token(self):
-        response = self.client.get("/api/search/autocomplete")
+        response = self.client.get("/api/search/autocomplete?search=food")
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
         self.assertEqual(
             response.json()["errors"],
             [{"code": ERROR_CODE_UNAUTHORIZED}],
+        )
+
+    def test_get_accounts_missing_search_param(self):
+        tokens_pair = RefreshToken.for_user(self.test_user)
+        access_token = str(tokens_pair.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+
+        response = self.client.get("/api/search/autocomplete")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(
+            response.json()["errors"],
+            [{"code": ERROR_CODE_MISSING_SEARCH_PARAMETER}],
         )
